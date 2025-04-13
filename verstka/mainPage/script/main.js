2f314d5e496368273c4c540c50c48c43ffe597ed
unknown <obertim@yandex.ru>
2025-04-13 13:46:24 +0400

    up full-catalog

--- verstka/mainPage/script/main.js
+++ verstka/mainPage/script/main.js
@@ -1,8 +1,8 @@
 // Full Catalog 
 
-let fullCatalogBtn = document.getElementById("fullCatalogBtn")
-let fullCatalog = document.getElementById("full__catalog__container")
-let fullCatalogExitBtn = document.getElementById("full__catalog__exit")
+const fullCatalogBtn = document.getElementById("fullCatalogBtn")
+const fullCatalog = document.getElementById("full__catalog__container")
+const fullCatalogExitBtn = document.getElementById("full__catalog__exit")
 
 fullCatalogBtn.addEventListener("click", () => {
     fullCatalog.classList.toggle("open")
@@ -11,3 +11,24 @@ fullCatalogBtn.addEventListener("click", () => {
 fullCatalogExitBtn.addEventListener("click", () => {
     fullCatalog.classList.toggle("open")
 })
+
+const categoryItems = document.querySelectorAll('.full__catalog__category__item');
+const catalogItems = document.querySelectorAll('.full__catalog__item');
+
+catalogItems.forEach((item, index) => {
+    if (index !== 0) {
+        item.classList.remove('open');
+    }
+});
+
+categoryItems.forEach((item, index) => {
+    item.addEventListener('click', function() {
+        catalogItems.forEach((catalogItem) => {
+            catalogItem.classList.remove('open');
+        });
+
+        if (catalogItems[index]) {
+            catalogItems[index].classList.toggle('open');
+        }
+    });
+});
